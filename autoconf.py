6deec93193227c1c81fbef46285ebdfd8f8e7921
Trzpiot <jakub.trzpiot@top-packaging.pl>
2021-05-18 16:01:42 +0200

    ver0.1

--- autoconf.py
+++ autoconf.py
@@ -1,1 +1,17 @@
 import json
+print("Podaj nazwe pliku")
+filename = input()
+file = json.load(open(filename+'.json', "r"))
+
+config = open("config.cfg", "w")
+for printer in file:
+
+    config.write('''
+        define host{
+	        use		    generic-printer
+	        host_name'''+printer+'''
+	        alias
+	        address		172.16.205+ip
+	        hostgroups	network-printers
+	        parents		TYC-PRINT-2
+        }''')
